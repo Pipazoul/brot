711dabebf840a2a4c3ac708e5365d6426f547283
ysiouda <ysiouda@erasme.org>
2019-01-23 14:18:20 +0100

    FEAT init word state checker (WIP)

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+$dictionary = "dictionaries/brotDb.txt";
+
+$state = "state.txt";
+
+$fh = fopen($dictionary,'r');
+$fileState = fopen($state,'r');
+$i = 0;
+$stateNb = fgets($fileState);
+echo $stateNb;
+while ($line = fgets($fh)) {
+  if($i >= $stateNb){
+    echo 'line number : '.$i. ' ';
+    file_put_contents($state, $i);
+    echo $line.'<br>';
+  }
+  $i++;
+
+}
+fclose($fh);
+fclose($filestate);
 
 
 ?>
